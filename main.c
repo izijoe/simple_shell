1a62887f98e06780e50cf6c6337cd7c27d231b70
izijoe <jospaco2001@yahoo.com>
2022-12-08 02:40:36 -0800

    simple shell

--- main.c
+++ main.c
@@ -13,9 +13,9 @@ int main(int ac, char **av)
 	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
 
 	if (ac == 2)
 	{
@@ -29,7 +29,7 @@ int main(int ac, char **av)
 				_eputs(av[0]);
 				_eputs(": 0: Can't open ");
 				_eputs(av[1]);
-				_eputchar('\n');
+				_eputchar(('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
